84a2369727ee5c89465242eca2fcf676cb7b4dc8
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2021-01-03 12:26:13 +0900

    categoryをcategory_idに変更

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_01_03_023433) do
     t.string "title", null: false
     t.string "text", null: false
     t.integer "price", null: false
-    t.integer "category", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
