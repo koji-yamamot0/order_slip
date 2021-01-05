90d4f350ead87749a0a77285f86803ccc193d84b
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2021-01-05 10:05:50 +0900

    cart_menuテーブルのマイグレーション

--- db/migrate/20210105005915_create_cart_menus.rb
+++ db/migrate/20210105005915_create_cart_menus.rb
@@ -1,7 +1,9 @@
 class CreateCartMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :cart_menus do |t|
-
+      t.references :cart, foreign_key: true
+      t.references :menu, foreign_key: true
+      t.integer :quantity
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_075141) do
+ActiveRecord::Schema.define(version: 2021_01_05_005915) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(version: 2021_01_04_075141) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cart_menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "menu_id"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_cart_menus_on_cart_id"
+    t.index ["menu_id"], name: "index_cart_menus_on_menu_id"
+  end
+
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -67,4 +82,6 @@ ActiveRecord::Schema.define(version: 2021_01_04_075141) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_menus", "carts"
+  add_foreign_key "cart_menus", "menus"
 end
