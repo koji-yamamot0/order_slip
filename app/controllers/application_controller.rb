a3dd2c12a856d9210a1099968a8e8c395c0a4b0f
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2021-01-06 09:32:07 +0900

    sessionを用いたカートの作成を記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  private
+  def current_cart
+    if Cart.find_by(id: session[:cart_id])
+      current_cart = Cart.find_by(id: session[:cart_id])
+    else
+      current_cart = Cart.create(customer_id: current_customer.id)
+    end
+    session[:cart_id] = current_cart.id
+    current_cart
+  end
 end
